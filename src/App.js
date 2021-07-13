76e4bf18b7ae032f01674bf98721ac962faded65
unknown <santhiya@1cloudhub.com>
2021-07-13 12:54:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div className="App">
       <h1> Job Scheduler...</h1>
+      <table>
+      <tr>
+          <th> h1 </th>
+          <th> h2 </th>
+          <th> h3 </th>
+        </tr>
+        <tr>
+          <td> 1 </td>
+          <td> 2 </td>
+          <td> 3 </td>
+        </tr>
+      </table>
     </div>
   );
 }
